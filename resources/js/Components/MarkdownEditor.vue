094739d67c459b63471bfd36a8db38f68f286584
Aditya Kunhare <honey.kunhare@gmail.com>
2025-06-01 10:26:16 +0530

    Added headings and link to the editor

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -90,6 +90,59 @@
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="16" height="16"  fill="currentColor"><path d="M5.75024 3.5H4.71733L3.25 3.89317V5.44582L4.25002 5.17782L4.25018 8.5H3V10H7V8.5H5.75024V3.5ZM10 4H21V6H10V4ZM10 11H21V13H10V11ZM10 18H21V20H10V18ZM2.875 15.625C2.875 14.4514 3.82639 13.5 5 13.5C6.17361 13.5 7.125 14.4514 7.125 15.625C7.125 16.1106 6.96183 16.5587 6.68747 16.9167L6.68271 16.9229L5.31587 18.5H7V20H3.00012L2.99959 18.8786L5.4717 16.035C5.5673 15.9252 5.625 15.7821 5.625 15.625C5.625 15.2798 5.34518 15 5 15C4.67378 15 4.40573 15.2501 4.37747 15.5688L4.3651 15.875H2.875V15.625Z"></path></svg>
 				</button>
 			</li>
+			<li>
+				<button 
+					@click="() => promptUserForHref()"
+					:class="[
+						editor.isActive('link') ?'bg-gray-300 hover:bg-gray-300':'bg-gray-100 border hover:bg-gray-200' 
+					]"
+					class="rounded-tl p-2 font-bold "
+					type="button" 
+					title="Add Link"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" width="16px" fill="currentColor"><path d="M18.3638 15.5355L16.9496 14.1213L18.3638 12.7071C20.3164 10.7545 20.3164 7.58866 18.3638 5.63604C16.4112 3.68341 13.2453 3.68341 11.2927 5.63604L9.87849 7.05025L8.46428 5.63604L9.87849 4.22182C12.6122 1.48815 17.0443 1.48815 19.778 4.22182C22.5117 6.95549 22.5117 11.3876 19.778 14.1213L18.3638 15.5355ZM15.5353 18.364L14.1211 19.7782C11.3875 22.5118 6.95531 22.5118 4.22164 19.7782C1.48797 17.0445 1.48797 12.6123 4.22164 9.87868L5.63585 8.46446L7.05007 9.87868L5.63585 11.2929C3.68323 13.2455 3.68323 16.4113 5.63585 18.364C7.58847 20.3166 10.7543 20.3166 12.7069 18.364L14.1211 16.9497L15.5353 18.364ZM14.8282 7.75736L16.2425 9.17157L9.17139 16.2426L7.75717 14.8284L14.8282 7.75736Z"></path></svg>
+				</button>
+			</li>
+			<li>
+				<button
+					@click="() => editor.chain().focus().toggleHeading({level: 2}).run()"
+					:class="[
+						editor.isActive('heading', {level:2}) ?'bg-gray-300 hover:bg-gray-300':'bg-gray-100 border hover:bg-gray-200' 
+					]"
+					class="rounded-tl p-2 font-bold "
+					type="button" 
+					title="Heading 1"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M13 20H11V13H4V20H2V4H4V11H11V4H13V20ZM21.0005 8V20H19.0005L19 10.204L17 10.74V8.67L19.5005 8H21.0005Z"></path></svg>
+				</button>
+			</li>
+			<li>
+				<button 
+					@click="() => editor.chain().focus().toggleHeading({level: 3}).run()"
+					:class="[
+						editor.isActive('heading', {level: 3}) ?'bg-gray-300 hover:bg-gray-300':'bg-gray-100 border hover:bg-gray-200' 
+					]"
+					class="rounded-tl p-2 font-bold "
+					type="button" 
+					title="Heading 2"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M4 4V11H11V4H13V20H11V13H4V20H2V4H4ZM18.5 8C20.5711 8 22.25 9.67893 22.25 11.75C22.25 12.6074 21.9623 13.3976 21.4781 14.0292L21.3302 14.2102L18.0343 18H22V20H15L14.9993 18.444L19.8207 12.8981C20.0881 12.5908 20.25 12.1893 20.25 11.75C20.25 10.7835 19.4665 10 18.5 10C17.5818 10 16.8288 10.7071 16.7558 11.6065L16.75 11.75H14.75C14.75 9.67893 16.4289 8 18.5 8Z"></path></svg>							
+				</button>
+			</li>
+			<li>
+				<button 
+					@click="() => editor.chain().focus().toggleHeading({level: 4}).run()"
+					:class="[
+						editor.isActive('heading',{level: 4}) ?'bg-gray-300 hover:bg-gray-300':'bg-gray-100 border hover:bg-gray-200' 
+					]"
+					class="rounded-tl p-2 font-bold "
+					type="button" 
+					title="Heading 3"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M22 8L21.9984 10L19.4934 12.883C21.0823 13.3184 22.25 14.7728 22.25 16.5C22.25 18.5711 20.5711 20.25 18.5 20.25C16.674 20.25 15.1528 18.9449 14.8184 17.2166L16.7821 16.8352C16.9384 17.6413 17.6481 18.25 18.5 18.25C19.4665 18.25 20.25 17.4665 20.25 16.5C20.25 15.5335 19.4665 14.75 18.5 14.75C18.214 14.75 17.944 14.8186 17.7056 14.9403L16.3992 13.3932L19.3484 10H15V8H22ZM4 4V11H11V4H13V20H11V13H4V20H2V4H4Z"></path></svg>
+				</button>
+			</li>
+			
 		</menu>
 		<EditorContent :editor="editor"/>
 	</div>
@@ -97,6 +150,7 @@
 <script setup>
 import { EditorContent, useEditor } from '@tiptap/vue-3';
 import Underline from '@tiptap/extension-underline'
+import Link from '@tiptap/extension-link';
 import { StarterKit } from '@tiptap/starter-kit';
 import { Markdown } from 'tiptap-markdown'
 import { watch } from 'vue';
@@ -109,9 +163,14 @@ let emit = defineEmits(['update:modelValue']);
 
 const editor = useEditor({
 	extensions: [
-		StarterKit,
+		StarterKit.configure({
+			heading: {
+				levels: [ 2,3,4 ]
+			}
+		}),
 		Markdown,
-		Underline
+		Underline,
+		Link
 	],
 	editorProps: {
 		attributes: {
@@ -127,4 +186,16 @@ watch(() => props.modelValue, (value) => {
 	}
 	editor.value?.commands.setContent(value)
 }, { immediate: true });
+
+let promptUserForHref = () => {
+	if(editor.value?.isActive('link')){
+		return editor.value?.chain().unsetLink().run();
+	}
+
+	let href = window.prompt('Where do you want to link?');
+	if (!href){
+		return editor.value?.chain().focus().run();
+	}
+	return editor.value?.chain().focus().setLink({href}).run();
+};
 </script>
